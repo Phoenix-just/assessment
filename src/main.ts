5f89bc2a69fe20fb051b75a3ae3d0d04a111992e
Phoenix-just <hemu63181indu@gmail.com>
2024-09-01 15:22:55 +0530

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { ENV } from './assets/environment';
+import { ENV } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .then(() => {
