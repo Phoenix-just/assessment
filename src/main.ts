c3612dcee9b5f9be522fb6443f53d6c74eeab873
Phoenix-just <hemu63181indu@gmail.com>
2024-09-01 15:17:33 +0530

    added environment file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ENV } from './assets/environment';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    if (ENV.enviroment == 'production') {
+      console.log = () => {}
+    }
+  })
   .catch((err) => console.error(err));
