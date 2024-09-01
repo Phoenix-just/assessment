22d45245f1efa7cedb0015442cde7f4dbb41be1e
Phoenix-just <hemu63181indu@gmail.com>
2024-09-01 09:11:26 +0530

    bug fixes

--- src/app/shared/editable-content.directive.ts
+++ src/app/shared/editable-content.directive.ts
@@ -52,28 +52,3 @@ export class EditableContentDirective implements ControlValueAccessor {
     this.onTouched = fn;
   }
 }
-// import { Directive, ElementRef, HostListener, Renderer2, forwardRef } from '@angular/core';
-// import { NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
-
-// @Directive({
-//   selector: '[appEditableContent]',
-//   standalone: true,
-//   providers: [
-//     {
-//       provide: NG_VALUE_ACCESSOR,
-//       useExisting: forwardRef(() => EditableContentDirective),
-//       multi: true
-//     }
-//   ]
-// })
-// export class EditableContentDirective {
-
-//   constructor(private el: ElementRef<any>, private ngModel: NgModel, private renderer: Renderer2) {
-//     this.renderer.setProperty(this.el.nativeElement, 'innerHTML', this.ngModel.control.value);
-//   }
-
-//   @HostListener('input')
-//   onInput() {
-//     this.ngModel.control.setValue(this.el.nativeElement.innerHTML);
-//   }
-// }
